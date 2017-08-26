9db9b01cd00d84dbe201f9a74c1b2c702393aff3
Blake Rollins <brollins90@gmail.com>
2017-08-25 18:41:34 -0600

    add linter, sorting and filename option

--- src/index.js
+++ src/index.js
@@ -1,52 +1,54 @@
+
 class WebpackChunkSizePlugin {
+    constructor(options) {
+        this.options = options || {};
+    }
 
-	constructor(context, request) {
-		if(!request) {
-			this.request = context;
-		} else {
-			this.context = context;
-			this.request = request;
-		}
-	}
-
-	apply(compiler) {
-		compiler.plugin("done", (stats) => {
-            const stats2 = stats.toJson({
-                performance: false,
-                hash: false,
-                version: false,
-                timings: false,
-                assets: true,
-                entrypoints: false,
-                chunks: false,
-                chunkModules: false,
-                chunkOrigins: false,
-                modules: false,
-                depth: false,
-                cached: false,
-                cachedAssets: false,
-                reasons: false,
-                usedExports: false,
-                providedExports: false,
-                optimizationBailout: false,
-                children: false,
-                source: false,
-            }).assets.map((asset) => {
-                return {
-                    cleanName: stripHash(asset.name),
-                    ...asset,
-                };
-            });
-            require("fs").writeFileSync(
-                path.join(__dirname, "..", "stats.json"),
-                JSON.stringify(stats2));
-		});
-	}
+    apply(compiler) {
+        compiler.plugin("done", (stats) => {
+            if (this.options.filename) {
+                const stats2 = stats.toJson({
+                    performance: false,
+                    hash: false,
+                    version: false,
+                    timings: false,
+                    assets: true,
+                    entrypoints: false,
+                    chunks: false,
+                    chunkModules: false,
+                    chunkOrigins: false,
+                    modules: false,
+                    depth: false,
+                    cached: false,
+                    cachedAssets: false,
+                    reasons: false,
+                    usedExports: false,
+                    providedExports: false,
+                    optimizationBailout: false,
+                    children: false,
+                    source: false,
+                }).assets.map((asset) => {
+                    if (asset.emitted) {
+                        return {
+                            file: stripHash(asset.name),
+                            size: asset.size,
+                        };
+                    }
+                }).sort((a, b) => {
+                    return a.file.toLowerCase().localeCompare(b.file.toLowerCase());
+                });
+                require("fs").writeFileSync(
+                    this.options.filename,
+                    JSON.stringify(stats2),
+                );
+            }
+        });
+    }
 }
 
 function stripHash(filename) {
     var idx = filename.indexOf('?');
-    return filename.substring(0, idx);
+    return (idx === -1) ? filename : filename.substring(0, idx);
 }
 
 module.exports = WebpackChunkSizePlugin;
